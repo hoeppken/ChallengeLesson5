ed25b65adf9c107ecb9a25f65476e7114f172bb4
hoeppken <rafael_rsv@hotmail.com>
2025-02-05 21:06:18 +0100

    code was clarified with some notes

--- ChallengeLesson5/ContentView.swift
+++ ChallengeLesson5/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     
     
-    // This provides a empty List to start with, the @State is needed when the property is linked/used in the UI View
+    // This provides a empty List to start with, the @State is needed when the property is linked/used in the UI View. @State is also important cause UI updates autom. when words array changes
     @State var words: [String] = []
     
     // Now create the array that contains all the words you want
@@ -24,15 +24,16 @@ struct ContentView: View {
         VStack {
             
     //in the VStack place the List
+    //The id: \.self part tells SwiftUI that each item in the words array is uniquely identified by its own value (which works because strings are unique and hashable).
             
             List(words, id: \.self) { word in
-                
+    //this is how is gonna be displayed
                 Text(word)
                 
                }
             
             Button("Tap here"){
-                
+    //here u creta an instance of the addRandomWord function
                 addRandomWord( )
                 
             }
@@ -44,6 +45,8 @@ struct ContentView: View {
     func addRandomWord () {
         
         if let randomWord = fruits.randomElement() {
+            // the .append add the const. ramdomWord to the words array
+            
             words.append(randomWord)
         }
         
